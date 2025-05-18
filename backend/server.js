36e67111cd3e4f38a5ac2d635bc509a4983ded76
Alena <yaalena0904@gmail.com>
2025-05-18 22:19:56 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -8,27 +8,27 @@ const newsRoutes = require('./routes/news');
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 // CORS Middleware
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://course-client-server.vercel.app/'
-];
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'https://course-client-server.vercel.app/'
+// ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// };
 
-app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
